dc9ed5c048037bdbbe824095c394a659473dabd2
zyue <zhuyue_25@163.com>
2017-10-13 11:45:04 +0800

    添加项目文件。

---
+++ DataDictionaryGenerator/Helper/ReturnMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDictionaryGenerator
+{
+    public struct ReturnMessage
+    {
+        public bool isSuccess { get; set; }
+        public string Messages { get; set; }
+
+        public ReturnMessage(string Msg, bool isSucc)
+        {
+            this.isSuccess = isSucc;
+            this.Messages = Msg;
+        }
+    }
+}
